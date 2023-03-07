aff367125945a93667aa827a4fba54b1b603116e
Horváth Dániel <119402648+HorvathVajda@users.noreply.github.com>
2023-03-07 08:54:05 +0100

    Add files via upload

---
+++ csaladfa.sql
@@ -0,0 +1,4 @@
+CREATE TABLE hazassag(primary key hazastars, hazassag_id INTEGER, datum DATE, helyszin VARCHAR(40));
+CREATE TABLE hazastars(primary key szemely, hazastars_id INTEGER, hazassag_id INTEGER, szemelyi_id INTEGER);
+CREATE TABLE szemely(szemelyi_id INTEGER, szuletes DATE, elhalalozas DATE, szuletesi_hely text, foglalkozas text, neme enum(férfi, nő), vezeteknev VARCHAR(40), keresztnev VARCHAR(40));
+CREATE TABLE szulok(primary key szemely, szulo_id INTEGER, szemelyi_id INTEGER, szulo_szemelyi_id INTEGER, kapcsolat enum(apa, anya));
