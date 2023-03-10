71d6347203cf7cfa908496d00b64ffdf6c9c6afd
Horváth Dániel <119402648+HorvathVajda@users.noreply.github.com>
2023-03-10 11:01:08 +0100

    Update csaladfa.sql

--- csaladfa.sql
+++ csaladfa.sql
@@ -1,39 +1,42 @@
-/*segíts már mert elakadtam nem tudom mi a hiba mondd meg és kijavítom xd*/
-drop table if exists hazassag;
-drop table if exists hazastars;
-drop table if exists szemely;
-drop table if exists szulok;
+DROP TABLE IF EXISTS hazassag;
+DROP TABLE IF EXISTS hazastars;
+DROP TABLE IF EXISTS szemely;
+DROP TABLE IF EXISTS szulok;
+
 CREATE TABLE hazassag(
     hazassag_id INTEGER,
     datum DATE,
     helyszin VARCHAR(40),
-    primary key (hazassag_id)
+    PRIMARY KEY (hazassag_id)
 );
-CREATE TABLE hazastars(
-    hazastars_id INTEGER,
-    hazassag_id INTEGER,
-    szemelyi_id integer,
-    primary key (hazastars_id),
-    foreign key (hazassag_id) references hazassag(hazassag_id),
-    foreign key (szemelyi_id) references szemely(szemelyi_id)
-);
+
 CREATE TABLE szemely(
     szemelyi_id INTEGER,
     szuletes DATE,
     elhalalozas DATE,
-    szuletesi_hely text,
-    foglalkozas text,
-    neme ENUM(férfi, nő),
+    szuletesi_hely TEXT,
+    foglalkozas TEXT,
+    neme ENUM('férfi', 'nő'),
     vezeteknev VARCHAR(40),
     keresztnev VARCHAR(40),
-    primary key (szemelyi_id)
+    PRIMARY KEY (szemelyi_id)
 );
+
+CREATE TABLE hazastars(
+    hazastars_id INTEGER,
+    hazassag_id INTEGER,
+    szemelyi_id INTEGER,
+    PRIMARY KEY (hazastars_id),
+    FOREIGN KEY (hazassag_id) REFERENCES hazassag(hazassag_id),
+    FOREIGN KEY (szemelyi_id) REFERENCES szemely(szemelyi_id)
+);
+
 CREATE TABLE szulok(
     szulo_id INTEGER,
     szemelyi_id INTEGER,
     szulo_szemelyi_id INTEGER,
-    kapcsolat ENUM(apa, anya),
-    primary key (szulo_id),
-    foreign key (szemelyi_id) references szemely(szemelyi_id),
-    foreign key (szulo_szemelyi_id) references szemely(szulo_szemelyi_id)
+    kapcsolat ENUM('apa', 'anya'),
+    PRIMARY KEY (szulo_id),
+    FOREIGN KEY (szemelyi_id) REFERENCES szemely(szemelyi_id),
+    FOREIGN KEY (szulo_szemelyi_id) REFERENCES szemely(szemelyi_id)
 );
