2f88b2509f4f8186bf761d0e6f403add3fac2e9f
Horváth Dániel <119402648+HorvathVajda@users.noreply.github.com>
2023-03-10 08:16:14 +0100

    Update csaladfa.sql

--- csaladfa.sql
+++ csaladfa.sql
@@ -1,4 +1,8 @@
-CREATE TABLE hazassag(primary key hazastars, hazassag_id INTEGER, datum DATE, helyszin VARCHAR(40));
-CREATE TABLE hazastars(primary key szemely, hazastars_id INTEGER, hazassag_id INTEGER, szemelyi_id INTEGER);
-CREATE TABLE szemely(szemelyi_id INTEGER, szuletes DATE, elhalalozas DATE, szuletesi_hely text, foglalkozas text, neme enum(férfi, nő), vezeteknev VARCHAR(40), keresztnev VARCHAR(40));
-CREATE TABLE szulok(primary key szemely, szulo_id INTEGER, szemelyi_id INTEGER, szulo_szemelyi_id INTEGER, kapcsolat enum(apa, anya));
+drop table if exists hazassag;
+drop table if exists hazastars;
+drop table if exists szemely;
+drop table if exists szulok;
+CREATE TABLE hazassag(hazassag_id INTEGER, datum DATE, helyszin VARCHAR(40), primary key (hazassag_id));
+CREATE TABLE hazastars(hazastars_id INTEGER, hazassag_id INTEGER, szemelyi_id integer, primary key (hazastars_id), foreign key (hazassag_id), foreign key (szemelyi_id) references szemely(szemelyi_id));
+CREATE TABLE szemely(szemelyi_id INTEGER, szuletes DATE, elhalalozas DATE, szuletesi_hely text, foglalkozas text, neme ENUM(férfi, nő), vezeteknev VARCHAR(40), keresztnev VARCHAR(40), primary key (szemelyi_id));
+CREATE TABLE szulok(szulo_id INTEGER, szemelyi_id INTEGER, szulo_szemelyi_id INTEGER, kapcsolat ENUM(apa, anya), primary key (szulo_id), foreign key (szemelyi_id), foreign key (szulo_szemelyi_id));
