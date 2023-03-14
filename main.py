f314663f113e1862e7488991350e20b7837da9b8
Nagy Gábor 1 <NagyGabor1@pg.local>
2023-03-14 09:28:05 +0100

    ezjolett

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import sqlite3 as sqlite
-
 con = sqlite.connect('szemely.db')
 
+#Szótárba rendezés
 szemelyek = []
-
 with con:
   cur = con.cursor()
   cur.execute('SELECT * FROM szemely')
   for person in cur.fetchall():
     id,csaladnev, keresztnev,szuletes,szulhey,foglalkozas = person
     szemelyek.append({ 'id' : id,'csaladnev' : csaladnev,'keresztnev' : keresztnev,'szuletes' : szuletes,'szulhey' : szulhey,'foglalkozas' : foglalkozas})
-
 print(szemelyek)
 
 
 hazassagok = []
-
 with con:
   cur = con.cursor()
   cur.execute('SELECT * FROM hazassag')
@@ -24,24 +21,21 @@ with con:
     hazassagok.append({'id' : id,'fej' : fej,'feleseg' : feleseg,'eskuvoido' : eskuvoido,'eskuvohely' : eskuvohely})
 print(hazassagok)
 
-#Név bekérése
-csaladn = input("Írja be a keresendő szemely családnevét: ")
-keresztn = input("Írja be a keresendő személy keresztnevét: ")
-'''
-sql = 'SELECT csaladnev,keresztnev,szuletes,foglalkozas FROM szemely WHERE csaladnev = ? and keresztnev = ?'
-nev = (csaladn , keresztn,)
-with con:
-  cur = con.cursor()
-  cur.execute(sql, nev)
-  for person in cur.fetchall():
-    print(person)'''
 
+#Név bekérése:
+csaladn = input("Családnevét: ")
+keresztn = input("Keresztnevét: ")
+
+
+#faj és egyébb adatok keresése:
 sorszam=0
 for i in range(len(szemelyek)):
   if csaladn == szemelyek[i]['csaladnev'] and keresztn == szemelyek[i]['keresztnev']:
+    print(f"  |--> születés helye-ideje: {szemelyek[i]['szuletes']} - {szemelyek[i]['szulhey']}")
+    print(f"  |--> foglalkozás: {szemelyek[i]['foglalkozas']} ")
     sorszam = szemelyek[i]['id']
 
-neme = "nincs"
+neme = ""
 for i in range(len(hazassagok)):
   if sorszam == hazassagok[i]['fej']:
     neme = "ferfi"
@@ -49,19 +43,19 @@ for i in range(len(hazassagok)):
     neme = "no"
 
 
-#Házastárs keresése
+#Házastárs keresése:
 if neme == "ferfi":
   for i in range(len(szemelyek)):
     for j in range(len(hazassagok)):
       if sorszam == hazassagok[j]['fej'] and hazassagok[j]['feleseg'] == szemelyek[i]['id']:
-        print(f"feleseg : {szemelyek[i]['csaladnev']} {szemelyek[i]['keresztnev']}")
+        print(f"  ˙--> feleség : {szemelyek[i]['csaladnev']} {szemelyek[i]['keresztnev']}")
 
 
 if neme == "no":
   for i in range(len(szemelyek)):
     for j in range(len(hazassagok)):
       if sorszam == hazassagok[j]['feleseg'] and hazassagok[j]['fej'] == szemelyek[i]['id']:
-        print(f"férj : {szemelyek[i]['csaladnev']} {szemelyek[i]['keresztnev']}")
+        print(f"  ˙--> férj : {szemelyek[i]['csaladnev']} {szemelyek[i]['keresztnev']}")
 
 
 
